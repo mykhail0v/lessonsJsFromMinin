799725c493025e78d14b02e1e2874f77e0f3c872
Nikita <nikitacom048@gmail.com>
2025-06-19 12:09:26 +0300

    first commit

---
+++ 01_base/app.js
@@ -0,0 +1,8 @@
+const resultElement = document.getElementById("result")
+// console.log(resultElement.textContent)
+const input1 = document.getElementById("input1")
+const input2 = document.getElementById("input2")
+
+const sum = Number(input1.value) + Number(input2.value)
+resultElement.textContent =  sum
+console.log(typeof sum)
