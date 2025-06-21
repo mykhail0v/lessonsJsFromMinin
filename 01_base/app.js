a680530498234438ef08dacdeae99a44221e9b24
Nikita <nikitacom048@gmail.com>
2025-06-21 23:46:09 +0300

    finished the calculator and basic theory

--- 01_base/app.js
+++ 01_base/app.js
@@ -1,8 +1,66 @@
-const resultElement = document.getElementById("result")
+const resultElement = document.getElementById("result");
 // console.log(resultElement.textContent)
-const input1 = document.getElementById("input1")
-const input2 = document.getElementById("input2")
+const input1 = document.getElementById("input1");
+const input2 = document.getElementById("input2");
+const submitBtn = document.getElementById("submit");
+const plusBtn = document.getElementById("plus");
+const minusBtn = document.getElementById("minus");
+const multiplyBtn = document.getElementById("multiply")
+const divisionBtn = document.getElementById("division")
 
-const sum = Number(input1.value) + Number(input2.value)
-resultElement.textContent =  sum
-console.log(typeof sum)
+let action = "+";
+
+// console.log(typeof sum)
+
+plusBtn.onclick = function () {
+  action = "+";
+};
+
+minusBtn.onclick = function () {
+  action = "-";
+};
+
+multiplyBtn.onclick = function () {
+  action = "x"
+}
+
+divisionBtn.onclick = function () {
+  action = "%"
+}
+
+function printResult(result) {
+ if (result < 0) {
+        resultElement.style.color = "red"
+    } else {
+        resultElement.style.color = "green"
+    }
+    resultElement.textContent = result;
+}
+
+function computeNumbersWithAction(inp1, inp2, actionSymbol) {
+  const num1 = Number(inp1.value)
+  const num2 = Number(inp2.value)
+  if (actionSymbol == "+") {
+    return num1 + num2
+  } else if (actionSymbol == "-") {
+    return num1 - num2
+  } else if (actionSymbol == "x") {
+    return num1 * num2
+  } else if (actionSymbol == "%") {
+    return num1 / num2
+  }
+
+}
+
+submitBtn.onclick = function () {
+const result = computeNumbersWithAction(input1, input2, action )
+printResult(result)
+  // if (action == "+") {
+  //   const sum = Number(input1.value) + Number(input2.value);
+  //   printResult(sum)
+  // } else  if (action == "-") {
+  //   const sum = Number(input1.value) - Number(input2.value);
+  //   printResult(sum)
+
+  // }
+};
---
+++ 01_base/theory.js
@@ -0,0 +1,36 @@
+// ================= Operators
+
+let a = 10
+let b = 5
+let c = 32
+
+// c = c + a
+// c = c - a
+// c = c * a
+// c = c / a
+
+// c += a
+// c -= a
+// c *= a
+// c /= a
+
+// c = c**2
+ 
+// =================== Data Types
+const age = 24
+const name = "Nikita"
+const isProgrammer = true
+let x
+console.log(typeof x)
+
+console.log(c)
+
+// ================== Priority
+
+//  console.log(2 >= 3)
+
+ const num1 = 42
+ const num2 = "42"
+
+ console.log(num1 === num2)
+ console.log(num1 == num2)
